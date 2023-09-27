f406c54a683a2a01633d13783d14fcf31f8890c8
jean-baptiste.aubry@ac-besancon.fr <jean-baptiste.aubry@ac-besancon.fr>
2023-09-27 15:09:15 +0200

    Correction Avec droits administrateurs

--- src/Vue/Vue_Menu_Administration.php
+++ src/Vue/Vue_Menu_Administration.php
@@ -18,9 +18,8 @@ class Vue_Menu_Administration extends Vue_Composant
              <nav id='menu'>
               <ul id='menu-closed'>  
                 <li><a href='?case=Gerer_catalogue'>Catalogue</a></li>   
-             <li><a href='?case=Gerer_entreprisesPartenaires'>Entreprises partenaires</a></li>
-               <li><a href='?case=Gerer_Commande'>Commandes</a></li>
-            
+                <li><a href='?case=Gerer_entreprisesPartenaires'>Entreprises partenaires</a></li>
+                <li><a href='?case=Gerer_Commande'>Commandes</a></li>
                 <li><a href='?case=Gerer_monCompte'>Mon compte</a></li> 
                </ul>
             </nav> 
